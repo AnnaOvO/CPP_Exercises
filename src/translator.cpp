0f5805fb61bc20d3c2ba48f957fbaf6c2c1389fd
AnnaOvO <annawangchen0310@gmail.com>
2023-01-24 11:04:33 +0100

    test

--- src/translator.cpp
+++ src/translator.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
+#include <set>
+#include <string>
+
+using namespace std;
+
+set<string> make_exit_commands()
+{
+    return {"q", "quit", "e", "exit"};
+}
 
 int main()
 {
     while (true)
     {
+        string tmp;
         std::cout << "This is a translator, but it does nothing for now..." << std::endl;
-        std::cin.ignore();
+        cin >> tmp;
+        auto c = make_exit_commands();
+        if (auto search = c.find(tmp); search != c.end())
+        {
+            break;
+        }
+        
     }
 
     return 0;
