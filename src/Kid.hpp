a555e86f7ca914053264a10d2c7892ecefb1db29
AnnaOvO <annawangchen0310@gmail.com>
2023-06-19 17:48:37 +0200

    update

--- src/Kid.hpp
+++ src/Kid.hpp
@@ -1,1 +1,31 @@
 #pragma once
+#include <string>
+#include <ostream>
+
+class Kid
+{
+    public:
+    Kid(const std::string& name, int money)
+    : _name {name}
+    , _money {money}
+    {}
+
+    const std::string& get_name() const
+    {
+        return _name;
+    }
+
+    int get_money() const
+    {
+        return _money;
+    }
+
+    private:
+    std::string _name;
+    int _money;
+};
+
+std::ostream& operator<<(std::ostream& stream, const Kid& person)
+{
+    return stream << person.get_name() << " has " << person.get_money() << " euros.";
+}
--- src/Plush.hpp
+++ src/Plush.hpp
@@ -1,1 +1,25 @@
 #pragma once
+class Plush{
+    public:
+    Plush()
+        : _cout { 10 }
+    {
+        
+    }
+    Plush(int value)
+        : _cout { value }
+    {
+        
+    }
+    int get_cost() const
+    {
+        return _cout;
+    }
+    
+    void set_cost(int n)
+    {
+        _cout = n;
+    }
+    private:
+    int _cout;
+};
